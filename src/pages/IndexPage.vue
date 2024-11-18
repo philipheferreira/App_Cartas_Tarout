a993c12ebaa322d415ecb7318b9ed1f84f23742a
philipheferreira <philipheferreira@gmail.com>
2024-11-18 13:57:44 -0300

    Feat: flip-left command

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="flip-container" :class="{ 'flipped': isFlipped }" @click="isFlipped = !isFlipped">
+      <img alt="Quasar logo" src="~assets/quasar-logo-vertical.svg" style="width: 200px; height: 200px">
+    </div>
+
+
   </q-page>
 </template>
 
@@ -12,6 +12,22 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data() {
+    return {
+      isFlipped: false
+    };
+  }
 })
 </script>
+
+<style>
+.flip-container {
+  display: inline-block;
+  transition: transform 0.5s ease-in-out;
+}
+
+.flipped {
+  transform: rotateY(180deg);
+}
+</style>
