7bb05c429291cba771fa42e53f594bf6cd57eced
philipheferreira <philipheferreira@gmail.com>
2024-11-21 09:32:45 -0300

    Create: Component cards

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,7 @@
 <template>
   <q-page class="flex flex-center">
 
-    <div class="flip-container" @click="isFlipped = !isFlipped">
-      <div class="flipper" :class="{ 'flipped': isFlipped }">
-        <div class="front">
-          <!-- Imagem da frente -->
-          <img
-            src="https://scontent.fbel7-1.fna.fbcdn.net/v/t39.30808-6/300499300_419109840322194_7374735255376892673_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGmSPFXJKbQqtAniqfSPv5yAkfiE9LL6CgCR-IT0svoKEtCNf09bmDYKoOarwVoYrKQvfaoLuV3RX0UzOA5kgOl&_nc_ohc=eOc6LWIoejwQ7kNvgF7SuUV&_nc_zt=23&_nc_ht=scontent.fbel7-1.fna&_nc_gid=AAxpzxjTv8bbWPlszbVUUkm&oh=00_AYC9uvRvEa8eTganjkdRwgjim1xnp0WGddwyTvEO2BjB8Q&oe=67415313"
-            alt="Imagem Frente" style="width: 240px; height: 400px" />
-        </div>
-        <div class="back">
-          <!-- Imagem de trás -->
-          <img src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*Yn5_HWunnZcaPqtrXzc2eQ.jpeg"
-            alt="Imagem Verso" style="width: 240px; height: 400px" />
-        </div>
-      </div>
-    </div>
+    <cartas></cartas>
 
 
   </q-page>
@@ -23,52 +9,16 @@
 
 <script>
 import { defineComponent } from 'vue'
+import cartas from 'components/Carta.vue'
 
 export default defineComponent({
   name: 'IndexPage',
   data() {
-    return {
-      isFlipped: false
-    };
+  },
+  components: {
+    cartas
   }
 })
 </script>
 
-<style>
-.flip-container {
-  perspective: 1000px;
-  /* Cria um efeito 3D */
-  cursor: pointer;
-}
-
-.flipper {
-  position: relative;
-  width: 300px;
-  /* Defina a largura desejada */
-  height: 200px;
-  /* Defina a altura desejada */
-  transform-style: preserve-3d;
-  transition: transform 0.5s ease-in-out;
-}
-
-.flipper.flipped {
-  transform: rotateY(180deg);
-}
-
-.front,
-.back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-}
-
-.front {
-  z-index: 2;
-  transform: rotateY(0deg);
-}
-
-.back {
-  transform: rotateY(180deg);
-}
-</style>
+<style></style>
