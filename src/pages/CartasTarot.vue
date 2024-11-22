f228c270a3b13e4452e140f059434a94fdec2c00
philipheferreira <philipheferreira@gmail.com>
2024-11-22 10:37:15 -0300

    Feat: adjusts

--- src/pages/CartasTarot.vue
+++ src/pages/CartasTarot.vue
@@ -3,114 +3,114 @@
     <div class="text-body1 text-center q-ma-md ">Os 22 Arcanos Maiores</div>
 
     <div class="flex">
-      <div class="q-my-xl q-mx-lg colunm">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-louco.jpg" />
-        <div class="text-body2 text-center">O Louco</div>
+        <div class="text-body2 text-center textoCartas">O Louco</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-mago.jpg" />
-        <div class="text-body2 text-center">O Mago</div>
+        <div class="text-body2 text-center textoCartas">O Mago</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg ">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-sacerdotisa.jpg" />
-        <div class="text-body2 text-center">A Sacerdotisa</div>
+        <div class="text-body2 text-center textoCartas">A Sacerdotisa</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-imperatriz.jpg" />
-        <div class="text-body2 text-center">A Imperatriz</div>
+        <div class="text-body2 text-center textoCartas">A Imperatriz</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-imperador.jpg" />
-        <div class="text-body2 text-center">O Imperador</div>
+        <div class="text-body2 text-center textoCartas">O Imperador</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-papa.jpg" />
-        <div class="text-body2 text-center">O Papa</div>
+        <div class="text-body2 text-center textoCartas">O Papa</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/os-enamorados.jpg" />
-        <div class="text-body2 text-center">Os Enamorados</div>
+        <div class="text-body2 text-center textoCartas">Os Enamorados</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-carro.jpg" />
-        <div class="text-body2 text-center">O Carro</div>
+        <div class="text-body2 text-center textoCartas">O Carro</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-forca.jpg" />
-        <div class="text-body2 text-center">A forca</div>
+        <div class="text-body2 text-center textoCartas">A forca</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-eremita.jpg" />
-        <div class="text-body2 text-center">O Eremita</div>
+        <div class="text-body2 text-center textoCartas">O Eremita</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-roda-da-fortuna.jpg" />
-        <div class="text-body2 text-center">A Roda da Fortuna</div>
+        <div class="text-body2 text-center textoCartas textoCartas">A Roda da Fortuna</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-justica.jpg" />
-        <div class="text-body2 text-center">A Justiça</div>
+        <div class="text-body2 text-center textoCartas">A Justiça</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-enforcado.jpg" />
-        <div class="text-body2 text-center">O Enforcado</div>
+        <div class="text-body2 text-center textoCartas">O Enforcado</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-morte.jpg" />
-        <div class="text-body2 text-center">A Morte</div>
+        <div class="text-body2 text-center textoCartas">A Morte</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-temperanca.jpg" />
-        <div class="text-body2 text-center">A Temperança</div>
+        <div class="text-body2 text-center textoCartas">A Temperança</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-diabo.jpg" />
-        <div class="text-body2 text-center">O Diabo</div>
+        <div class="text-body2 text-center textoCartas">O Diabo</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-torre.jpg" />
-        <div class="text-body2 text-center">A Torre</div>
+        <div class="text-body2 text-center textoCartas">A Torre</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-estrela.jpg" />
-        <div class="text-body2 text-center">A Estrela</div>
+        <div class="text-body2 text-center textoCartas">A Estrela</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/a-lua.jpg" />
-        <div class="text-body2 text-center">A Lua</div>
+        <div class="text-body2 text-center textoCartas">A Lua</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-sol.jpg" />
-        <div class="text-body2 text-center">O Sol</div>
+        <div class="text-body2 text-center textoCartas">O Sol</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-julgamento.jpg" />
-        <div class="text-body2 text-center">O Julgamento</div>
+        <div class="text-body2 text-center textoCartas">O Julgamento</div>
       </div>
 
-      <div class="q-my-xl q-mx-lg">
+      <div class="q-my-lg q-mx-lg">
         <img class="imagemCartas" src="https://www.astrolink.com.br/data/tarot/o-mundo.jpg" />
-        <div class="text-body2 text-center">O Mundo</div>
+        <div class="text-body2 text-center textoCartas">O Mundo</div>
       </div>
 
     </div>
@@ -126,7 +126,11 @@ export default {
 
 <style>
 .imagemCartas {
-  height: 126px;
-  width: 70px;
+  height: 160px;
+  width: 100px;
+}
+
+.textoCartas {
+  max-width: 100px;
 }
 </style>
